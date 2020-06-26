f14b7da10382998b1867472459b6b73e20ada638
Monika Tercjak <monika.tercjak@gmail.com>
2020-06-26 17:14:25 +0200

    class inconsistencies fixed

--- python_files/general_functions.py
+++ python_files/general_functions.py
@@ -3,35 +3,13 @@ import numpy as np
 from general_classes import *
 
 
-class GRS80:
-    a_axis = 6378137
-    e2 = 0.00669438002290
-    f = 1 / 298.25722210
-    b_axis = a_axis - a_axis * f
-    e2p = round((a_axis ** 2 - b_axis ** 2) / b_axis ** 2, 13)
-
-    e4 = e2 ** 2
-    e6 = e2 ** 3
-    A0 = 1 - e2 / 4 - 3 * e4 / 64 - 5 * e6 / 256
-    A2 = 3 / 8 * (e2 + e4 / 4 + 15 * e6 / 128)
-    A4 = 15 / 256 * (e4 + 3 * e6 / 4)
-    A6 = 35 * e6 / 3072
-
-
-class WGS84:
-    a_axis = 6378137
-    e2 = 0.00669437999014
-    f = 1 / 298.257223563
-    b_axis = a_axis - a_axis * f
-    e2p = round((a_axis ** 2 - b_axis ** 2) / b_axis ** 2, 13)
-
-    e4 = e2 ** 2
-    e6 = e2 ** 3
-    A0 = 1 - e2 / 4 - 3 * e4 / 64 - 5 * e6 / 256
-    A2 = 3 / 8 * (e2 + e4 / 4 + 15 * e6 / 128)
-    A4 = 15 / 256 * (e4 + 3 * e6 / 4)
-    A6 = 35 * e6 / 3072
-
+def as2rad(arc_seconds):
+    '''
+    Remark: works for both scalar values and matrices
+    :param arc_seconds: angle in arcseconds
+    :return: angle in radians
+    '''
+    return arc_seconds * np.pi / 180 / 3600
 
 def validate_coordinates(angle):
     return 0 <= angle <= 2 * mt.pi
@@ -46,13 +24,6 @@ def deg2rad(angle):
     return angle * np.pi / 180
 
 
-def as2rad(arc_seconds):
-    '''
-    Remark: works for both scalar values and matrices
-    :param arc_seconds: angle in arcseconds
-    :return: angle in radians
-    '''
-    return arc_seconds * np.pi / 180 / 3600
 
 
 def degrees_to_dms(degrees):
